f32f920a21259094269795b05abdbf9114eaee20
topimvestnigeria <topinvestnigeria@gmail.com>
2026-07-12 23:43:13 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,61 @@
-// Firebase SDK
+// =====================================
+// TOP v2 Firebase Configuration
+// =====================================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Firebase configuration
+
+// =====================================
+// Firebase Config
+// =====================================
+
 const firebaseConfig = {
-  apiKey: "AIzaSyATY9siExVCe0snihq75eCzBi4BZ3aj_lU",
-  authDomain: "topapp-a6897.firebaseapp.com",
-  projectId: "topapp-a6897",
-  storageBucket: "topapp-a6897.firebasestorage.app",
-  messagingSenderId: "219264849420",
-  appId: "1:219264849420:web:4c5b3918e2606041d38029"
+
+    apiKey: "AIzaSyATY9siExVCe0snihq75eCzBi4BZ3aj_lU",
+
+    authDomain: "topapp-a6897.firebaseapp.com",
+
+    projectId: "topapp-a6897",
+
+    storageBucket: "topapp-a6897.firebasestorage.app",
+
+    messagingSenderId: "219264849420",
+
+    appId: "1:219264849420:web:4c5b3918e2606041d38029"
+
 };
 
+
+// =====================================
 // Initialize Firebase
+// =====================================
+
 const app = initializeApp(firebaseConfig);
 
-// Services
+
+// =====================================
+// Firebase Services
+// =====================================
+
 const auth = getAuth(app);
+
 const db = getFirestore(app);
 
-export { app, auth, db };
+
+// =====================================
+// Export Services
+// =====================================
+
+export {
+
+    app,
+
+    auth,
+
+    db
+
+};
