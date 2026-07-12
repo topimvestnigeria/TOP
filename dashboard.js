3bc9fe3adf38c9eaa8986c90584302d6193905ee
topimvestnigeria <topinvestnigeria@gmail.com>
2026-07-12 15:53:15 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,44 @@
+import { auth, db } from "./firebase.js";
 
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+    try {
+
+        const userRef = doc(db, "users", user.uid);
+        const userSnap = await getDoc(userRef);
+
+        if (!userSnap.exists()) {
+            alert("User account not found.");
+            return;
+        }
+
+        const data = userSnap.data();
+
+        document.getElementById("welcomeUser").innerText =
+            `Welcome, ${data.fullname} 👋`;
+
+        document.getElementById("balance").innerText =
+            "₦" + Number(data.balance).toLocaleString();
+
+    } catch (error) {
+
+        console.error(error);
+        alert(error.message);
+
+    }
+
+});
